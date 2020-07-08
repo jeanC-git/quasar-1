7d7a8d120da6d00c45ba518070753ae4443b0449
Jean Carlo <jeanca2198@gmail.com>
2020-07-08 17:56:59 -0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-light-blue-14 glossy text-white">
+        <q-btn
+          flat
+          round
+          dense
+          icon="menu"
+          class="q-mr-sm"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+        <q-avatar>
+          <img src="https://somadevoos.com/public_image/favicon.png" />
+        </q-avatar>
+
+        <q-toolbar-title>R8vn</q-toolbar-title>
+
+        <q-btn flat round dense icon="whatshot" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+      <q-list>
+        <q-item-label header class="text-grey-8">Sobre mí</q-item-label>
+        <EssentialLink v-for="link in menu" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+export default {
+  name: "MainLayout",
+
+  components: {
+    EssentialLink
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+      menu: [
+        {
+          title: "Fb/r8vn.fb",
+          caption: "fb/r8vn.fb",
+          icon: "mdi-facebook",
+          link: "https://www.facebook.com/r8vn.fb"
+        },
+        {
+          title: "Github/jeanC-git",
+          caption: "github.com/jeanC-git",
+          icon: "mdi-code-tags",
+          link: "https://github.com/jeanC-git"
+        },
+        {
+          title: "SOMA",
+          caption: "somadevoos.com",
+          icon: "mdi-link-variant",
+          link: "https://somadevoos.com"
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="q-px-lg q-pb-md">
+    <q-timeline :layout="layout" class="secondary">
+      <q-timeline-entry heading>Productos Apay</q-timeline-entry>
+      <q-timeline-entry
+        v-for="item in data"
+        :key="item.id"
+        :title="item.nombre"
+        :subtitle="'Precio: '+item.precio"
+        :avatar="item.foto"
+        :body="item.descripcion"
+      ></q-timeline-entry>
+    </q-timeline>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  computed: {
+    layout() {
+      return this.$q.screen.lt.sm
+        ? "dense"
+        : this.$q.screen.lt.md
+        ? "comfortable"
+        : "loose";
+    }
+  },
+  data() {
+    return {
+      arrayTimeline: [
+        {
+          title: "Event Title Heading",
+          entries: [
+            {
+              title: "Event title 1",
+              subtitle: "subtitle 1",
+              side: "left",
+              avatar: "https://cdn.quasar.dev/img/avatar5.jpg",
+
+              text:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            },
+            {
+              title: "Event title 2",
+              subtitle: "subtitle 1",
+              side: "right",
+              icon: "mdi-code-tags",
+              text:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }
+          ]
+        },
+        {
+          title: "Event Title Heading 2",
+          entries: [
+            {
+              title: "Event title 2 1",
+              subtitle: " subtitle 2 1",
+              side: "left",
+              icon: "done_all",
+              text:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }
+          ]
+        }
+      ],
+      data: []
+    };
+  },
+  created() {
+    this.getApiData();
+  },
+  methods: {
+    getApiData() {
+      let vue = this;
+      vue.$axios
+        .get("http://apay.somadevoos.com/api/apiProducto")
+        .then(response => {
+          vue.data = response.data.data;
+        });
+    }
+  }
+};
+</script>
